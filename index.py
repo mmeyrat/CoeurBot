04eaf9cb43eba2f8c4b46dfdf8f950a1f0722c67
mmeyrat <maximemeyrat@orange.fr>
2022-08-30 22:52:51 +0200

    add points count when commenting

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import json
 from twitchio.ext import commands
 from dotenv import load_dotenv
 
@@ -20,7 +21,18 @@ class Bot(commands.Bot):
 		# Messages with echo set to True are messages sent by the bot
 		if message.echo:
 			return
-		print(message.content)
+
+		with open("data.json", "r") as f:
+			data = json.load(f)
+
+		if message.author.name not in data.keys():
+			data[message.author.name] = { "points": 0 }
+
+		data[message.author.name]["points"] += 10
+
+		with open("data.json", "w") as f:
+			json.dump(data, f)
+
 		await self.handle_commands(message)
 
 	def emoteSpam(self, text, count):
@@ -44,7 +56,7 @@ class Bot(commands.Bot):
 	@commands.command()
 	async def clip(self, ctx: commands.Context):
 		partial_user = self.create_user(streamer_id, streamer_name)
-		clip = await partial_user.create_clip(token=os.environ["TOKEN"])
+		clip = await partial_user.create_clip(token = os.environ["TOKEN"])
 		await ctx.send(f"Clip crée et dispo ici : {clip['edit_url'].replace('/edit', '')}")
 
 bot = Bot()
